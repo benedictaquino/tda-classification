061ba3f38d65b589dc329e4e85330b53cb0e80ec
Benedict Aquino <benaq9@gmail.com>
2019-04-24 01:06:45 -0400

    added some class skellies
    SimplicalComplex, Chain, and some functions/methods (not implemented
    yet)

--- tda_utilities/topology.py
+++ tda_utilities/topology.py
@@ -3,11 +3,11 @@
 class Simplex:
     """Defines a k-simplex object --- vertices can be any object"""
     def __init__(self, *args):
-        self.k = len(args) - 1 # k-simplex
-        self.points = args
+        self.points = tuple(set(args))
+        self.dim = len(self.points) - 1 # k-simplex
     
     def __len__(self) -> int:
-        return self.k
+        return self.dim
 
     def __getitem__(self, i: int) -> object:
         return self.points[i]
@@ -15,33 +15,57 @@ class Simplex:
     def __repr__(self):
         return f'{self.k}-simplex: {self.points}'
 
+    def __lt__(self, other):
+        return len(self) < len(other)
+
 class SimplicialComplex:
     """Defines a simplicial complex"""
-    # TODO: all of this
+    def __init__(self, simplices):
+        self.simplices = list(set(simplices))
+        self.simplices.sort()
+        self.dim = len(max(self.simplices))
+
+    def __len__(self) -> int:
+        return self.dim
+
+    def __getitem__(self, i) -> Simplex:
+        return self.simplices[i]
+
+    def __lt__(self, other):
+        return len(self) < len(other)
+
+    def chain(self, k):
+        """returns the k-chain"""
+        raise NotImplementedError
+
+class Chain:
+    """Defines a k-Chain"""
     def __init__(self):
-        pass
-    # I want to calculate simplicial k-chains, boundaries, cycles, etc. from
-    # this class --- not sure yet if they should be methods or functions
+        raise NotImplementedError
 
-def closure(S: set) -> SimplicialComplex:
-    """return the closure of the complex"""
-    pass
+    def boundary(self):
+        """returns a (k-1)-Chain"""
+        raise NotImplementedError
 
-def star(S: set) -> SimplicialComplex:
-    """return the star of the complex"""
-    pass
+def closure(simplices: set) -> SimplicialComplex:
+    """return the closure of the set of simplices"""
+    raise NotImplementedError
 
-def link(S: set) -> SimplicialComplex:
-    """return the link of the complex"""
-    pass
+def star(simplices: set) -> SimplicialComplex:
+    """return the star of the set of simplices"""
+    raise NotImplementedError
+
+def link(simplices: set) -> SimplicialComplex:
+    """return the link of the set of simplices"""
+    raise NotImplementedError
 
 class Filtration:
     """Filtration object
     """
     # TODO: All of this I guess
     def __init__(self):
-        self.complex_list = []
+        raise NotImplementedError
 
     def create_filtration(self):
-        pass
+        raise NotImplementedError
 
