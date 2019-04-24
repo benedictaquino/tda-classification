0f45892e38329375af86d708d79a973922d4e969
Benedict Aquino <benaq9@gmail.com>
2019-04-24 09:13:02 -0400

    changes to Simplex and began SimplicialChain
    Simplex faces attribute added created at initialization, which is
    assigned the faces of the simplex which are initialized recursively
    
    Added orientation attribute to Simplex to work with SimplicialChain
    
    initializing a SimplicialChain should take a SimplicialComplex, a dimension
    parameter, and an orientation parameter
    
    the dimension argument passed in should decide which simplicial k-chain
    to create from the complex
    
    the orientation parameter should be some truthy or non-truthy value
    deciding which orientation of the simplices to use

--- tda_utilities/topology.py
+++ tda_utilities/topology.py
@@ -1,32 +1,48 @@
 """Topological Feature Selection Functions"""
+from itertools import combinations
 
 class Simplex:
-    """Defines a k-simplex object --- vertices can be any object"""
-    def __init__(self, *args):
-        self.points = tuple(set(args))
-        self.dim = len(self.points) - 1 # k-simplex
+    """Defines a k-simplex object"""
+    def __init__(self, *points, orientation: int = None):
+        """Simplex takes any arguments as points that can make up a k-simplex"""
+        self.k = len(set(points)) - 1 # k-simplex
+        self.orientation = orientation
+        self.points = points
+        # recursively initialize arguments as simplices such that each
+        # argument passed becomes a 0-simplex
+        if self.k == 0: 
+            self.faces = None
+        else:
+            self.faces = tuple( [Simplex(*combo) for combo in combinations(set(points), self.k)])
     
     def __len__(self) -> int:
-        return self.dim
+        return self.k
 
     def __getitem__(self, i: int) -> object:
         return self.points[i]
 
     def __repr__(self):
-        return f'{self.k}-simplex: {self.points}'
+        if self.orientation:
+            return f'{self.k}-simplex with orientation {self.orientation}: {" ".join(str(point) for point in self.points)}'
+        return f'{self.k}-simplex: {" ".join(str(point) for point in self.points)}'
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         return len(self) < len(other)
+
+    def orient(self, sigma: int): 
+        """assigns an orientation to the simplex"""
+        self.orientation = sigma
 
 class SimplicialComplex:
     """Defines a simplicial complex"""
     def __init__(self, simplices):
-        self.simplices = list(set(simplices))
-        self.simplices.sort()
-        self.dim = len(max(self.simplices))
+        # -> set to remove duplicates -> back to list
+        self.simplices = list(set(simplices)) 
+        self.simplices.sort() # puts lower-dimensional simplices first
+        self.k = len(max(self.simplices)) # largest dimension simplex
 
     def __len__(self) -> int:
-        return self.dim
+        return self.k
 
     def __getitem__(self, i) -> Simplex:
         return self.simplices[i]
@@ -34,14 +50,19 @@ class SimplicialComplex:
     def __lt__(self, other):
         return len(self) < len(other)
 
+    def __repr__(self, other):
+        return f'simplicial {self.k}-complex'
+
     def chain(self, k):
         """returns the k-chain"""
         raise NotImplementedError
 
-class Chain:
-    """Defines a k-Chain"""
-    def __init__(self):
-        raise NotImplementedError
+class SimplicialChain:
+    """Defines a simplicial k-chain"""
+    def __init__(self, complex: SimplicialComplex, orientation: int = 0):
+        # TODO: ???
+        self.chain = None 
+        self.orienation = orientation # this should do something, but what?
 
     def boundary(self):
         """returns a (k-1)-Chain"""
