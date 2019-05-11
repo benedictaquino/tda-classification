a1e2d93eed20f13214f27f8e33fe10f921958689
Benedict Aquino <benaq9@gmail.com>
2019-05-10 20:58:03 -0400

    improve readability of load_data.py
    using Path from pathlib instead of os.path module to enable script to
    always load the data relative to the location of the load_data.py script
    itself --- script is now much less verbose and more readable

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,9 +1,9 @@
-import os
 import struct
+from pathlib import Path
 import numpy as np
 
 
-def read(data: str = "train") -> (np.array, np.array):
+def read_mnist(data: str = 'train') -> (np.array, np.array):
     '''
     This function reads the MNIST training or testing dataset ubyte file to a
     numpy array
@@ -11,48 +11,35 @@ def read(data: str = "train") -> (np.array, np.array):
     Parameters
     ----------
     data : string designating train or test data sets
+
     Returns
     -------
     labels, images : a tuple of numpy arrays containing the training data
     '''
-    if data == "train":
-        label_filepath = "data/train-labels-idx1-ubyte"
-        image_filepath = "data/train-images-idx3-ubyte"
-    elif data == "test":
-        label_filepath = "data/t10k-labels-idx1-ubyte"
-        image_filepath = "data/t10k-images-idx3-ubyte"
-    else:
-        raise ValueError("Invalid argument")
+    # get path to data directory relative to this script's location
+    directory = Path(__file__).parent.parent / 'data'
 
-    with open(label_filepath, "rb") as f:
-        struct.unpack(">II", f.read(8))
+    if data == 'train':  # load training set
+        label_filepath = directory / 'train-labels-idx1-ubyte'
+        image_filepath = directory / 'train-images-idx3-ubyte'
+    elif data == 'test':  # load testing set
+        label_filepath = directory / 't10k-labels-idx1-ubyte'
+        image_filepath = directory / 't10k-images-idx3-ubyte'
+    else:  # raise error
+        raise ValueError('Invalid argument')
+
+    with open(label_filepath, 'rb') as f:
+        struct.unpack('>II', f.read(8))
         labels = np.fromfile(f, dtype=np.uint8)
 
-    with open(image_filepath, "rb") as f:
-        tmp1, tmp2, rows, cols = struct.unpack(">IIII", f.read(16))
+    with open(image_filepath, 'rb') as f:
+        _, _, rows, cols = struct.unpack('>IIII', f.read(16))
         images = np.fromfile(f, dtype=np.uint8)\
             .reshape(len(labels), rows, cols)
 
     return images, labels
 
 
-if __name__ == "__main__":
-    try:
-        # Load training and test data to numpy arrays
-        train_images, train_labels = read("train")
-        test_images, test_labels = read("test")
-    except FileNotFoundError:  # change directory to base directory
-        filepath = os.path.abspath(__file__)
-        dname = os.path.abspath(os.curdir)
-        os.chdir(
-                os.path.abspath(
-                    os.path.join(
-                        filepath,
-                        os.path.pardir,
-                        os.path.pardir
-                        )))
-
-        train_labels, train_images = read("train")
-        test_labels, test_images = read("test")
-
-        os.chdir(dname)  # change back to current directory
+if __name__ == '__main__':
+    train_images, train_labels = read_mnist('train')
+    test_images, test_labels = read_mnist('test')
